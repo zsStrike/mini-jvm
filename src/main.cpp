e817b0dc85586791b0d117caf57860821c626396
zsStrike <2439758713@qq.com>
2023-04-06 17:04:36 +0800

    feat: finish args parse

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,38 @@
 #include <iostream>
+#include "boost/program_options.hpp"
+#include "boost/program_options/variables_map.hpp"
+
+namespace bpo = boost::program_options;
 
 int main(int argc, const char** argv) {
-    std::cout << "hello world" << std::endl;
+    std::string classpath;
+    std::vector<std::string> mainclass;
+
+    bpo::options_description desc("Allowed options:");
+    desc.add_options()
+        ("help,h", "display help message.")
+        ("version,v", "display version message.")
+        ("classpath,c", bpo::value<std::string>(&classpath), "classpath.")
+        ("mainclass", bpo::value<std::vector<std::string>>(&mainclass), "class and args")
+    ;
+    bpo::positional_options_description pdes;
+    pdes.add("mainclass", -1);
+    bpo::variables_map vmap;
+    bpo::store(bpo::command_line_parser(argc, argv).options(desc).positional(pdes).run(), vmap);
+    bpo::notify(vmap);
+
+    if (vmap.count("help") == 1) {
+        std::cout << desc << std::endl;
+    } else if (vmap.count("version") == 1) {
+        std::cout << "version is 1.0" << std::endl;
+    } else {
+        std::cout << "mini-jvm: 1.0 started." << std::endl;
+        std::cout << "classpath: " << classpath << std::endl;
+        for (auto& arg : mainclass) {
+            std::cout << arg << " ";
+        }
+        std::cout << std::endl;
+    }
+    
+    return 0;
 }
