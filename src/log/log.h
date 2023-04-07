b8e8a730c001c76a5f6dcda1cae967d11d4111f6
zsStrike <2439758713@qq.com>
2023-04-07 13:58:57 +0800

    fix: add __FILE__ in log utils

--- src/log/log.h
+++ src/log/log.h
@@ -2,11 +2,11 @@
 #include "boost/format.hpp"
 
 #define LOG_INFO(...)              \
-        log_info(__FUNCTION__, __LINE__, __VA_ARGS__);  
+        log_info(__FILE__, __FUNCTION__, __LINE__, __VA_ARGS__);  
 
-template<typename F, typename L, typename P, typename... Args>
-void log_info(const F& f, const L& l, const P& p, const Args&... args) {
-    std::cout << boost::format("[%1%@%2%]: ") % f % l;
+template<typename D, typename F, typename L, typename P, typename... Args>
+void log_info(const D& file, const F& fun, const L& l, const P& p, const Args&... args) {
+    std::cout << boost::format("[%1%:%2%@%3%]: ") % file % fun % l;
     boost::format fmt(p);
     (void) std::initializer_list<int>{([&] {fmt % args;}(), 0)...};
     std::cout << fmt.str() << std::endl;
