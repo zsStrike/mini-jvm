f9dcb5b630175a34d64021c2be7e0052dfb95d47
zsStrike <2439758713@qq.com>
2023-05-14 10:36:22 +0800

    doc: add chinese chars test in HelloWorld test

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
    public static void main(String[] args) {
+      System.out.println("中文测试，检测中文转换");
       System.out.println("Hello, world! Prepared to print args， argLen is");
       System.out.println(args.length);
       for (String arg : args) {
